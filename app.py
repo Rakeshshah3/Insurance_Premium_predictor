62473acf234aebe70ca833eb314350997f33e85e
Rakesh Sah <rakeshsah90346@gmail.com>
2026-02-26 17:46:49 +0530

    Initial FastAPI project for Railway deployment

---
+++ app.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from Schema.user_input import UserInput
+from model.predict import predict_output,model,MODEL_VERSION
+from Schema.prediction_response import PredictionResponse
+
+
+
+
+
+app = FastAPI()
+
+
+
+
+# Human readable       
+@app.get('/')
+def home():
+    return{'message':'Insurance preimum Prediction API'}
+@app.get('/health')
+
+# Machine readable
+def health_check():
+    return{
+
+
+        'status':'OK',
+        'version':MODEL_VERSION,
+        'model_loaded':model is not None
+    }        
+
+@app.post('/predict',response_model=PredictionResponse)
+def predict_premium(data: UserInput):
+
+    user_input ={
+        'bmi': data.bmi,
+        'age_group': data.age_group,
+        'lifestyle_risk': data.lifestyle_risk,
+        'city_tier': data.city_tier,
+        'income_lpa': data.income_lpa,
+        'occupation': data.occupation
+    }
+
+    try:
+     prediction = predict_output(user_input)
+     return JSONResponse(status_code=200, content={'response': prediction})
+    except Exception as e:
+       return JSONResponse(status_code=500,content=str(e))   
+  
+
+
---
+++ try.py
@@ -0,0 +1,9 @@
+{
+  "age": 19,
+  "weight": 65,
+  "height": 1.3,
+  "income_lpa": 10,
+  "smoker": true,
+  "city": "mumbai",
+  "occupation": "retired"
+}
